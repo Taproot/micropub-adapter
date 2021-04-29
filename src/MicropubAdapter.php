c8b83b0c52582d73f64e12818ba88dd7c8a9de61
Barnaby Walters <barnaby@waterpigs.co.uk>
2021-04-29 23:43:46 +0200

    Stubbed source file, tests

--- src/MicropubAdapter.php
+++ src/MicropubAdapter.php
@@ -1,2 +1,29 @@
-<?php
+<?php declare(strict_types=1);
 
+namespace Taproot\Micropub;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Config
+ * 
+ * 
+ */
+class Config {
+
+}
+
+/**
+ * Handle Request
+ * 
+ * Handle an incoming request to a webmention endpoint, dispatching it to the appropriate 
+ * callback defined in the $config parameter.
+ * 
+ * @param RequestInterface $request
+ * @param Config $config
+ * @return 
+ */
+function handleRequest(RequestInterface $request, Config $config) {
+	return;
+}
---
+++ tests/MicropubAdapterTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Taproot\Micropub\Tests;
+
+use Taproot\Micropub;
+
+use PHPUnit\Framework\TestCase;
+
+final class MicropubAdapterTest extends TestCase {
+	public function testConfigCanBeCreated() {
+		$config = new Micropub\Config;
+	}
+}

06522dfbb61488a159a65a6e2160576b9ad62dcb
Barnaby Walters <barnaby@waterpigs.co.uk>
2021-04-29 23:52:20 +0200

    Added phpDocumentor config, stub docs

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Taproot\\Micropub\\handleRequest\u0028\u0029",
+            "name": "handleRequest",
+            "summary": "Handle\u0020Request",
+            "url": "namespaces/taproot-micropub.html#function_handleRequest"
+        },                {
+            "fqsen": "\\Taproot\\Micropub\\Config",
+            "name": "Config",
+            "summary": "Config",
+            "url": "classes/Taproot-Micropub-Config.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Taproot\\Micropub",
+            "name": "Micropub",
+            "summary": "",
+            "url": "namespaces/taproot-micropub.html"
+        },                {
+            "fqsen": "\\Taproot",
+            "name": "Taproot",
+            "summary": "",
+            "url": "namespaces/taproot.html"
+        }            ]
+);
